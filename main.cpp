b34c899300e13c2a08c28d9d0e4ef8580cbed048
凌建发 <jianfa.ljf@alibaba-inc.com>
2020-12-28 00:44:45 +0800

    add build ci

--- main.cpp
+++ main.cpp
@@ -2,8 +2,32 @@
 #include "stdlib.h"
 #include "string.h"
 #include "stdio.h"
+#include "assert.h"
+
+
+bool test_cat()
+{
+#define CAT1 1
+#define CAT13 -13
+   int m12 = _H2PP_CAT2(1, 2);
+   if (m12 != 12) return false;
+
+   int m13 = _H2PP_CAT2(CAT1, 3);
+   if (m13 != -13) return false;
+
+   int n12 = H2PP_CAT2(1, 2);
+   if (n12 != 12) return false;
+
+   int n13 = H2PP_CAT2(CAT1, 3);
+   if (n13 != 13) return false;
+
+    return true;
+#undef CAT1
+#undef CAT13
+}
 
 int main()
 {
+    assert(test_cat());
     return 0;
 }
