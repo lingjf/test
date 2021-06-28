8a9e00039d1e6b3542fa780b28e75ea74b70b64b
凌建发 <jianfa.ljf@alibaba-inc.com>
2021-06-28 21:48:40 +0800

    vtable

--- main.cpp
+++ main.cpp
@@ -53,25 +53,25 @@ main(int argc, char* argv[])
 //   printf("%s\n", a.f1());
   printf("&A=%p, &A.a=%p", &a, &a.a);
   printf(", &A.f0=%p", &A::f0);
-  printf(", &A.f1=%d", &A::f1);
-  printf(", &A.f2=%d\n", &A::f2);
+  printf(", &A.f1=%p", &A::f1);
+  printf(", &A.f2=%p\n", &A::f2);
 
 //   printf("%s\n", b.f1());
   printf("&B=%p, &B.b=%p", &b, &b.b);
   printf(", &B.f0=%p", &B::f0);
-  printf(", &B.f1=%d", &B::f1);
-  printf(", &B.f2=%d\n", &B::f2);
+  printf(", &B.f1=%p", &B::f1);
+  printf(", &B.f2=%p\n", &B::f2);
 
 //   printf("%s\n", c.f1());
   printf("&C=%p, &C.c=%p", &c, &c.c);
   printf(", &C.f0=%p", &C::f0);
-  printf(", &C.f1=%d", &C::f1);
-  printf(", &C.f2=%d\n", &C::f2);
+  printf(", &C.f1=%p", &C::f1);
+  printf(", &C.f2=%p\n", &C::f2);
 
 //   printf("%s\n", d.f1());
   printf("&D=%p, &D.d=%p", &d, &d.d);
   printf(", &D.f0=%p", &D::f0);
-  printf(", &D.f1=%d", &D::f1);
-  printf(", &D.f2=%d\n", &D::f2);
+  printf(", &D.f1=%p", &D::f1);
+  printf(", &D.f2=%p\n", &D::f2);
   return 0;
 }
