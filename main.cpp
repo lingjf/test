9c26712c49ad3a35e352c2e5b52ebe75f9d28928
lingjf <lingjf@gmail.com>
2021-06-29 20:53:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,13 @@ void get1(void* addr)
    symbol->MaxNameLen = 256;
    if (!SymFromAddr(hProcess, (DWORD64)(addr), 0, symbol))
       return ;
-  ::printf("%s %p %lld \n",symbol->Name, symbol->Address, (long long)addr-(long long)symbol->Address);
+   unsigned long long offset=0;
+   if (memcmp("ILT+", symbol->Name, 4) == 0) {  // ILT (Incremental Link Table), JMP(e9)
+      offset = (unsigned long long)((unsigned char*)symbol->Address + 5 + *(long*)((unsigned char*)symbol->Address + 1));
+      
+   }
+   
+  ::printf("%s %p %lld %x \n",symbol->Name, symbol->Address, (long long)addr-(long long)symbol->Address, offset);
     
 }
 
